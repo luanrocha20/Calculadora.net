2c4ab12f188f3f942a3a3588a2ed789f30d6b076
Luan Rocha dos Santos <luanrocha.ios@gmail.com>
2022-07-17 14:54:32 -0300

    opcao saida adicionado

--- Program.cs
+++ Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 
 class Program{
-    enum Menu {Soma, Subtracao, Divisao, Multiplicacao, Potencia, Raiz, Sair }    
+    enum Menu {Soma = 1, Subtracao, Divisao, Multiplicacao, Potencia, Raiz, Sair }    
 
     static void Main(string [] args){
-        Console.WriteLine("Bem vindo a calculadora no Console, selecione uma das opcoes:");
-        Console.WriteLine("1- Soma\n2- Subtracao\n3- Divisao\n4- Multiplicacao\n5- Potencia\n6- Raiz\n7- Sair");
-        Console.ReadLine();
+        
+        bool saida = false;
+        
+        while(!saida){
+            Console.WriteLine("Bem vindo a calculadora no Console, selecione uma das opcoes:");
+            Console.WriteLine("1- Soma\n2- Subtracao\n3- Divisao\n4- Multiplicacao\n5- Potencia\n6- Raiz\n7- Sair");
+            //conversao de string em int por meio de casting
+            Menu opcao = (Menu)int.Parse(Console.ReadLine());
+            
+            switch(opcao){
+                case Menu.Sair:
+                    saida = true;
+                    break;
+            }
+            
+            Console.Clear();
+
+        }
+
         
     }
 }
