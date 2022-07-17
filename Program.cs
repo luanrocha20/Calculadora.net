5677b555bd96ec64819e4add64a588269405b03a
Luan Rocha dos Santos <luanrocha.ios@gmail.com>
2022-07-17 14:37:54 -0300

    Adicionado esboco menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program{
+    enum Menu {Soma, Subtracao, Divisao, Multiplicacao, Potencia, Raiz, Sair }    
+
+    static void Main(string [] args){
+        Console.WriteLine("Bem vindo a calculadora no Console, selecione uma das opcoes:");
+        Console.WriteLine("1- Soma\n2- Subtracao\n3- Divisao\n4- Multiplicacao\n5- Potencia\n6- Raiz\n7- Sair");
+        Console.ReadLine();
+        
+    }
+}
